a678b9cb5bc6750a0e4b23caa068aa1f72611cd7
Daniel Marquez Quintero <daniel.marquez@hahcompany.com>
2026-05-28 13:02:02 +0200

    Ticket form added.

---
+++ tickets/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from .models import Ticket
+
+class TicketForm(forms.ModelForm):
+    class Meta:
+        model = Ticket
+        fields = ['title', 'description', 'priority', 'category', 'status', 'created_by', 'created_at']
+        # Podemos añadir clases de CSS (como Bootstrap) para que se vea bien
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter ticket title'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 4, 'placeholder': 'Enter ticket description'}),
+            'priority': forms.Select(attrs={'class': 'form-select'}),
+            'category': forms.Select(attrs={'class': 'form-select'}),
+            'status': forms.Select(attrs={'class': 'form-select'}),
+            # WHEN WE HAVE A USER MODEL, UNCOMMENT THIS
+            #'created_by': forms.Select(attrs={'class': 'form-select'}),
+            'created_at': forms.DateTimeInput(attrs={'class': 'form-control', 'type': 'datetime-local'}),
+        }
